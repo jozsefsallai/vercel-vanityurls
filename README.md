994ca3db2234445c59c0fc8a853fc31b9972219d
József Sallai <jozsef@sallai.me>
2020-07-04 01:47:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vercel Vanity URLs for Go
+
+Vercel app to use custom domains for Go packages.
+
+<a href="https://vercel.com/new/project?template=jozsefsallai/vercel-vanityurls"><img width="128" src="https://vercel.com/button" alt="One-click Deployment" /></a>
+
+## FAQ
+
+**It says that I didn't set up my GitHub username! How do I do that?**
+
+Using the `VANITYURLS_GITHUB_USERNAME` environment variable.
+
+**I use something other than GitHub. What can I do?**
+
+Currently, there is no support for other Git sites, but this is a planned feature.
+
+## License
+
+MIT.
---
+++ api/handler.js
@@ -0,0 +1,40 @@
+function buildHTML(path, repo) {
+  return [
+    '<!doctype html>',
+    '<head>',
+    `<title>${path}</title>`,
+    `<meta name="go-import" content="${path} git ${repo}">`,
+    `<meta name="go-source" content="${path} _ ${repo}/tree/master{/dir} ${repo}/blob/master{/dir}/{file}#L{line}">`,
+    '</head>',
+    '<body>',
+    path,
+    '</body>',
+    '</html>'
+  ].join('');
+}
+
+module.exports = (req, res) => {
+  if (req.url && req.url.toLowerCase() === '/favicon.ico') {
+    res.statusCode = 204;
+    return res.end();
+  }
+
+  const username = process.env.VANITYURLS_GITHUB_USERNAME;
+
+  if (!username) {
+    return res.status(500).send('Vercel Vanity URLs error: GitHub username not provided.');
+  }
+
+  const package = req.url.slice(1);
+  if (!package.length) {
+    res.writeHead(301, {
+      Location: `https://github.com/${username}`
+    });
+    return res.end();
+  }
+
+
+  const path = `${req.headers.host}/${package}`;
+  const repo = `https://github.com/${username}/${package}`;
+  return res.send(buildHTML(path, repo));
+};
